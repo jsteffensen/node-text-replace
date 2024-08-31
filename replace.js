cee36e6b4f6767ab2fb170d27db39c4da4e557d8
jsteffensen <jsteffensen@gmail.com>
2024-08-31 18:13:11 +0200

    add more

--- replace.js
+++ replace.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 
-const file = '4.1_1';
+const file = '5.14_4';
 
 fs.readFile('dictionary.json', 'utf8', (err, data) => {
   if (err) {
@@ -20,12 +20,20 @@ fs.readFile('dictionary.json', 'utf8', (err, data) => {
 		
 	    expandedText = expandedText.replaceAll('(' + dictionary['pairs'][i]['abbreviation'] + ')', '');
 		expandedText = expandedText.replaceAll('(' + dictionary['pairs'][i]['abbreviation'] + 's)', '');
+		expandedText = expandedText.replaceAll(/\(see [^)]+\)/g, '');
+		expandedText = expandedText.replaceAll(/\(See [^)]+\)/g, '');
 		expandedText = expandedText.replaceAll(abbreviationPlural, expanded + 's');
 		expandedText = expandedText.replaceAll(abbreviation, expanded);
 	  }
 	  
+	  // bullet chars
+	  expandedText = expandedText.replace(/\uF0B7/g, '-');
+	  expandedText = expandedText.replace(/\no /g, '\n-- ');
+	  
 	  expandedText = expandedText.replaceAll('(s)', 's');
 	  expandedText = expandedText.replaceAll('(forthcoming)', '');
+	  expandedText = expandedText.replaceAll('including, but not limited to:', 'including:');
+	  expandedText = expandedText.replaceAll('(but not be limited to)', '');
 	  expandedText = expandedText.replaceAll('(draft)', '');
 	  expandedText = expandedText.replaceAll(' (e.g.,', ', for example ');
 	  expandedText = expandedText.replaceAll(' (e.g. ', ', for example ');
@@ -33,19 +41,30 @@ fs.readFile('dictionary.json', 'utf8', (err, data) => {
 	  
 	  expandedText = expandedText.replaceAll('USD(A&S)', 'Under Secretary of Defense for Acquisition and Sustainment');
 	  expandedText = expandedText.replaceAll('USD(R&E)', 'Under Secretary of Defense for Research and Engineering');
-	  expandedText = expandedText.replaceAll('MIL-HDBK-', 'military handbook ');
+	  expandedText = expandedText.replaceAll('Engineering/Engineering', 'Engineering');
+	  expandedText = expandedText.replaceAll('MIL-HDBK-', 'Military Handbook ');
+	  expandedText = expandedText.replaceAll('MIL-STD-', 'Military Standard ');
 	  
 	  expandedText = expandedText.replaceAll('  ', ' ');
 	  expandedText = expandedText.replaceAll('   ', ' ');
 	  
 	  expandedText = expandedText.replaceAll(' .', '.');
 	  expandedText = expandedText.replaceAll(' ,', ',');
+	  expandedText = expandedText.replaceAll('“', '"');
+	  expandedText = expandedText.replaceAll('”', '"');
+	  
+	  // puctuation after x.y.z
+	  expandedText = expandedText.replace(/(\n\d\.\d\.\d)/g, '$1.');
+	  
+	  // line break before new sentence
+	  expandedText = expandedText.replace(/([.!?])\s+(?=[A-Z])/g, '$1\n\n');
 	  
 		fs.writeFile(file + '.txt', expandedText, err => {
 		  if (err) {
 		    console.error(err);
 		  } else {
-		    console.log('complete\n\n');
+		    console.log(file + '\n');
+			console.log('complete\n');
 			
 			
 			//finad two or more capital letters next to each other to detect abbreviations that may not be in dictionary yet
